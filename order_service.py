a04e86e813efb9ce67e6382fadc2742aa15c4356
leolarrel <>
2021-10-24 21:04:20 +0800

    <Version>0.1 : order service for real

--- order_service.py
+++ order_service.py
@@ -1,4 +1,7 @@
 import sys
+import json
+import configparser
+from datetime import datetime as dt
 import logging
 import socketserver
 import threading
@@ -35,22 +38,35 @@ class ApiError(Exception) :
     pass
 
 class sinopac_shioaji_api :
-    def __init__ (self) :
+    def __init__ (self, simulate) :
+        logD("__init__()")
+
         try :
-            logD("__init__()")
-            self.__api__ = sj.Shioaji(simulation=True)
-        except:
+            with open("nearby_contract.json", "r") as f:
+                temp = json.load(f)
+
+            self.__nearby_contract_list__ = temp["nearby_contract_record"]
+            for i in self.__nearby_contract_list__ :
+                temp = dt.strptime(i["delivery"], "%Y/%m/%d %H:%M:%S")
+                i["timestamp"] = int(temp.timestamp())
+
+            self.simulate = True if simulate == "yes" else False
+            self.__api__ = sj.Shioaji(simulation = self.simulate)
+
+        except Exception as e :
+            logE(f"{e}")
             raise ApiEerror("failed to init")
 
-    def login(self) :
+    def login(self, arg_id, arg_passwd, arg_ca_path, arg_ca_passwd) :
         try :
             logD("login()")
-            self.__api__.login(person_id="PAPIUSER01",
-                    passwd="2222",
-                    contracts_cb=print)
-            #api.activate_ca(ca_path="/c/your/ca/path/Sinopac.pfx",
-            #        ca_passwd="YOUR_CA_PASSWORD",
-            #        person_id="Person of this Ca")
+            self.__api__.login(person_id = arg_id, \
+                               passwd = arg_passwd, \
+                               contracts_cb=print)
+            if self.simulate == False :
+                self.__api__.activate_ca(ca_path = arg_ca_path, \
+                                         ca_passwd = arg_ca_passwd, \
+                                         person_id = arg_id)
 
             #you can iter api.Contracts object to know contracts info
             #MXFR<1,2> is small TXF, TXFR<1,2> is big TXF
@@ -65,26 +81,39 @@ class sinopac_shioaji_api :
         logD("logout()")
         self.__api__.logout()
 
-    def order(self, buysell, contract, market, price, amount) :
-        logD(f"order(): {buysell}, {contract}, {market}, {price}, {amount}")
+    def verify_nearby_contract(self, contract) :
+        temp = dt.now().timestamp()
+        nearby_contract_name = ""
+        for i in self.__nearby_contract_list__ :
+            if temp < i["timestamp"] :
+                nearby_contract_name = "TXF" if contract == 'T' else "MXF"
+                nearby_contract_name += i["month"]
+                break
 
-        if market == 'L' and price == '' :
-            #print(f"failed to check argument. price can not be zero string if market is Limit")
-            raise ApiError
+        temp = self.__api__.Contracts.Futures.TXF if contract == 'T' else self.__api__.Contracts.Futures.MXF
+        for i in temp :
+            if nearby_contract_name == i.symbol :
+                return i
 
-        contract = \
-            self.__api__.Contracts.Futures.TXF.TXFR1 if contract == 'T' else self.__api__.Contracts.Futures.MXF.MXFR1
+        raise ApiError
+
+    def order(self, buysell, contract, market, price, position) :
+        logD(f"order(): {buysell}, {contract}, {market}, {price}, {position}")
+
+        real_contract = self.verify_nearby_contract(contract)
+
         order = self.__api__.Order( \
                 action = sj.constant.Action.Buy if buysell == 'b' else sj.constant.Action.Sell, \
                 price = int(price.strip()), \
-                quantity = amount, \
+                quantity = position, \
                 price_type = sj.constant.FuturesPriceType.MKP if market == 'M' else sj.constant.FuturesPriceType.LMT, \
-                order_type=sj.constant.FuturesOrderType.ROD, \
-                octype=sj.constant.FuturesOCType.Auto, \
-                account=self.__api__.futopt_account)
-        trade = self.__api__.place_order(contract, order)
+                order_type = sj.constant.FuturesOrderType.IOC if market == 'M' else sj.constant.FuturesOrderType.ROD, \
+                octype = sj.constant.FuturesOCType.Auto, \
+                account = self.__api__.futopt_account)
+
+        trade = self.__api__.place_order(real_contract, order)
         if trade.status.status == sj.constant.Status.Failed :
-            #print(f"failed to order. status code: {trade.status.status_code}")
+            logE(f"failed to order. status code: {trade.status.status_code}")
             raise ApiError
 
 class the_request_handler(socketserver.BaseRequestHandler) :
@@ -131,8 +160,14 @@ def main() :
     log_init()
 
     try:
-        api_obj = sinopac_shioaji_api()
-        api_obj.login()
+        ini_setting = configparser.ConfigParser()
+        ini_setting.read("setting.ini")
+
+        api_obj = sinopac_shioaji_api(ini_setting["global"]["simulation"])
+        api_obj.login(ini_setting["global"]["id"], \
+                      ini_setting["global"]["password"], \
+                      ini_setting["global"]["ca_file"], \
+                      ini_setting["global"]["ca_password"])
     except :
         logE(f"failed to create and login sinopac shioaji api. abort")
         return -1
